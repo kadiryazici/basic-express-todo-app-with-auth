ed777db42e8f1ab4d9b19cd160011303dbb6d35f
Kadir Yazıcı <47540799+kadiryazici@users.noreply.github.com>
2020-07-16 17:43:31 +0300

    improvements

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ $ npm run start
 | :------: | ------ | ----- | :-----: | :-----: | :----: |
 | <br>`POST`<br><br> | Login | /login | `username`<br> `password`  | `-` | `id` |
 | <br>`POST`<br><br> | SignUp | /signup | `username`<br> `password` | `-` | `id` | 
-| <br>`POST`<br><br> | Add Todo | /add | `text` | `authorization: id` | `-` | 
+| <br>`POST`<br><br> | Add Todo | /add | `text` | `authorization: id` | `id`<br> `isDone`<br> `text` | 
 | <br>`GET`<br><br> | Get Todos | /todos | `-` | `authorization: id` |  `id`<br> `isDone`<br> `text` | 
 | <br>`PUT`<br><br> | Update Todo | /update | `id`<br> `isDone: Boolean` | `authorization: id` | `id`<br> `isDone`<br> `text` | 
 | <br>`DELETE`<br><br> | Delete Todo | /delete | `id` | `authorization: id` | `-` | 
--- server/post/addTodo.js
+++ server/post/addTodo.js
@@ -1,5 +1,6 @@
 import db from "../../db/setUpDb.js";
 import { v4 as uuidv4 } from "uuid";
+import { json } from "body-parser";
 
 export default (req, res) => {
     const auth = req.headers.authorization;
@@ -9,17 +10,18 @@ export default (req, res) => {
     if (auth && isauth) {
         if (body.text) {
             try {
+                const value = {
+                    id: uuidv4(),
+                    isDone: false,
+                    text: body.text
+                }
                 db.get('users')
                     .find({ id: auth })
                     .get('todos')
-                    .push({
-                        id: uuidv4(),
-                        isDone: false,
-                        text: body.text
-                    })
+                    .push(value)
                     .write();
 
-                res.status(200).send('{ status: "success" }');
+                res.status(200).send(JSON.stringify(value));
             } catch (error) {
                 res.status(401).send('{ status: "error" }');
             }
