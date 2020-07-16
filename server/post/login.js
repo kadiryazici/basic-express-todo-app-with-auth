48752a1be0a67d0d9838d5c55f8bd6eecfd4f2c0
Kadir Yazıcı <47540799+kadiryazici@users.noreply.github.com>
2020-07-16 15:41:53 +0300

    Fixed Login

--- server/post/login.js
+++ server/post/login.js
@@ -3,9 +3,9 @@ import db from "../../db/setUpDb.js";
 export default (req, res) => {
     const body = req.body;
     if (body.username && body.password) {
-        const isUser = db.get('users').find({ name: body.username, password: body.password }).value();
-        if (isUser) {
-            res.status(200).send(JSON.stringify({ id: isUser.id }))
+        const user = db.get('users').find({ name: body.username, password: body.password }).value();
+        if (user) {
+            res.status(200).send(JSON.stringify({ id: user.id }))
         } else {
             res.status(401).send(JSON.stringify({ status: "wrong password or username" }));
         }
